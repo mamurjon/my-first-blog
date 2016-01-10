d8dbd96d976d2db04217997f5e9fbae3957a9fdd
Mamurjon <kadirovmamur95@mail.ru>
2016-01-10 19:04:24 +0500

    My Django app, first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mamur/Documents/Sublime_Text/djangoproject/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ polls/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
+import datetime
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Question(models.Model):
+    # ...
+
+
+
+    #def was_published_recently(self):
+     #   return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+    
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
